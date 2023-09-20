d8f6af11078b2732b8f158807277e3fc3385c49f
Alina Agnistova <agnistova1@gmail.com>
2023-09-20 20:06:51 +0300

    fixed: validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.getElementById('valForm').addEventListener('submit', function (e) {
     e.preventDefault();
     let x = document.querySelectorAll('input[type="radio"]:checked')[0].value;
-    let y = document.getElementById('y').value;
-    let r = document.getElementById('r').value;
+    let y = document.getElementById('y');
+    let r = document.getElementById('r');
     if (validate(x, y, r)) {
         send(x, y, r);
     }
@@ -36,14 +36,16 @@ function send(x, y, r) {
 
 
 
-function showExc(message) {
-    let alert = document.getElementById('alert');
-    let alertDiv = document.createElement('div');
-    alertDiv.textContent = message;
-    alertDiv.style.color = 'red';
-    alert.appendChild(alertDiv);
+function showError(element,message) {
+    const errorElement = document.createElement('div');
+    errorElement.classList.add('error-message');
+    errorElement.textContent = message;
+    errorElement.style.color = 'red';
+    errorElement.style.fontSize = '20px';
+    errorElement.style.textAlign = 'left';
+    element.parentNode.insertBefore(errorElement, element.nextSibling);
     setTimeout(function () {
-        alertDiv.remove();
+        errorElement.remove();
     }, 2000);
 
 
@@ -51,40 +53,40 @@ function showExc(message) {
 
 function validate(x, y, r) {
     let replaceDot = val => val.replace(',', '.');
-    let y1 = replaceDot(y);
-    let r1 = replaceDot(r);
+    let y1 = replaceDot(y.value);
+    let r1 = replaceDot(r.value);
     if (x == null){
-        showExc("Необходимо выбрать значение координаты X:(");
+        showError(x,"Необходимо выбрать значение координаты X:(");
         return false;
     }
 
     if (y1 === "") {
-        showExc("Необходимо указать значение координаты Y:(");
+        showError(y,"Необходимо указать значение координаты Y:(");
         return false;
     }
 
     if (isNaN(y1)) {
-        showExc("Нет, так не надо. Надо вот так: Y - число");
+        showError(y,"Нет, так не надо. Надо вот так: Y - число");
         return false;
     }
 
     if (y1 < -5 || y1 > 5) {
-        showExc("Координата Y может быть любым числом из диапазона {-5;5}");
+        showError(y,"Y может быть любым числом из {-5;5}");
         return false;
     }
 
     if (r1 === "") {
-        showExc("Необходимо указать значение радиуса:(");
+        showError(y,"Необходимо указать значение радиуса:(");
         return false;
     }
 
     if (isNaN(r1)) {
-        showExc("Нет, так не надо. Надо вот так: Радиус - число");
+        showError(r,"Нет, так не надо. Надо вот так: R - число");
         return false;
     }
 
     if (r1 < 1 || r1 > 4) {
-        showExc("Радиус может быть любым числом из диапазона {1;4}");
+        showError(r,"R может быть любым числом из {1;4}");
         return false;
     }
     return true;
